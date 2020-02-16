d83f8022717c3afd76d2339b858f1e0ce9c1727a
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-02-16 21:40:40 +0700

    using jwt

--- api_example/settings.py
+++ api_example/settings.py
@@ -124,5 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly']
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
 }
